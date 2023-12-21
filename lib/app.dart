649202245c32424a7576ecde122dea9a9f67aa5d
Richar Cangui <richarsantiagocangui96@gmail.com>
2023-12-20 21:11:17 -0500

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:componentes/screens/home_screen.dart';
+import 'package:componentes/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      routes: routes,
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,7 @@
+import 'package:componentes/screens/home_screen.dart';
+
+/// Crear nuestras rutas
+
+final routes = {
+  HomeScreen.routeName : (context) => const HomeScreen(),
+};
