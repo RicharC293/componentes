579d4a30478696f3d66202725a6229062a65358b
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-08 20:57:02 -0500

    Refactor card screen layout

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -7,6 +7,36 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(title: const Text("Card")),
+        body: Center(
+          child: Card(
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.photo_album),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Título de la tarjeta'),
+                        Text('Este es el subtitulo o descripción de la tarjeta, Este es el subtitulo o descripción de la tarjeta, Este es el subtitulo o descripción de la tarjeta, Este es el subtitulo o descripción de la tarjeta'),
+                        SizedBox(height: 10),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: TextButton(onPressed: () {}, child: const Text('Aceptar'))),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
