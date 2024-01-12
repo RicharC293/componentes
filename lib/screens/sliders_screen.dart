bee9ee26b2707fbe2d1b5b11c0ee3b4c070b2f4e
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-11 20:05:50 -0500

    Add sliders and image filters

--- lib/screens/sliders_screen.dart
+++ lib/screens/sliders_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SlidersScreen extends StatefulWidget {
@@ -12,11 +15,15 @@ class SlidersScreen extends StatefulWidget {
 class _SlidersScreenState extends State<SlidersScreen> {
   // double valueSlider1 = 0.5;
   late double valueSlider1;
+  late double valueSlider2;
+  late double valueSlider3;
 
   @override
   void initState() {
     super.initState();
-    valueSlider1 = 0.1;
+    valueSlider1 = 1.0;
+    valueSlider2 = 2.0;
+    valueSlider3 = 0.0;
   }
 
   @override
@@ -25,14 +32,21 @@ class _SlidersScreenState extends State<SlidersScreen> {
       appBar: AppBar(title: const Text("Sliders")),
       body: Column(
         children: [
-          Transform.scale(
-            scale: valueSlider1,
-            child: Image.asset('assets/image.png', height: 300),
+          ImageFiltered(
+            imageFilter:
+                ImageFilter.dilate(radiusX: valueSlider3, radiusY: valueSlider3),
+            child: Transform.rotate(
+              angle: valueSlider2 * pi,
+              child: Transform.scale(
+                scale: valueSlider1,
+                child: Image.asset('assets/image.png', height: 300),
+              ),
+            ),
           ),
           Slider(
             value: valueSlider1,
             onChanged: (value) {
-              print(value);
+              // print(value);
               setState(() {
                 valueSlider1 = value;
               });
@@ -40,7 +54,32 @@ class _SlidersScreenState extends State<SlidersScreen> {
             thumbColor: Colors.red,
             activeColor: Colors.yellow,
             inactiveColor: Colors.black,
-          )
+          ),
+          Slider(
+            min: 0,
+            max: 2,
+            value: valueSlider2,
+            onChanged: (value) {
+              /// 0   ---> 0
+              /// 0.5 ---> 90
+              /// 1   ---> 180
+              /// 1.5 ---> 270
+              /// 2   ---> 360
+              setState(() {
+                valueSlider2 = value;
+              });
+            },
+          ),
+          Slider(
+            max: 3,
+            value: valueSlider3,
+            onChanged: (value) {
+              // print(value);
+              setState(() {
+                valueSlider3 = value;
+              });
+            },
+          ),
         ],
       ),
     );
