f1588f41a68d778716a06a3f77bd60be6edc1c1c
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-12 20:28:19 -0500

    Add infinite scroll functionality

--- lib/screens/infinity_scroll_screen.dart
+++ lib/screens/infinity_scroll_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:componentes/services/services.dart';
 import 'package:componentes/widgets/image_card.dart';
 import 'package:flutter/material.dart';
@@ -12,25 +14,169 @@ class InfinityScrollScreen extends StatefulWidget {
 }
 
 class _InfinityScrollScreenState extends State<InfinityScrollScreen> {
+  List<String>? _images;
+  late int initialImages;
+  late int amount;
+
+  bool _isLoading = false;
+
+  /// Crear un controlador de scroll
+  late ScrollController _scrollController;
+
+  /// Ejecutar un método previo a la primera renderización
   @override
   void initState() {
     super.initState();
-    // _getImages();
+    initialImages = 0;
+    amount = 5;
+    _scrollController = ScrollController();
+    _scrollController.addListener(_listener);
+    _getImages();
+  }
+
+  void _listener() {
+    print("LISTENER");
+    if (_scrollController.position.pixels + 50 >=
+            _scrollController.position.maxScrollExtent &&
+        !_isLoading) {
+      print("PUEDES HACER FETCH");
+      initialImages = initialImages + amount;
+      _getImages();
+    }
   }
 
   Future<void> _getImages() async {
-    final images = await Services().getImages(0, 10);
-    print(images);
+    try {
+      print("InitialImages: $initialImages");
+      setState(() {
+        _isLoading = true;
+      });
+      final fetchImages = await Services().getImages(initialImages, amount);
+
+      /// Spread operator
+      /// _images = ['1','2','3']
+      /// fetchImages = ['4','5','6']
+      /// [...['1','2','3'], ...['4','5','6']]
+      /// ['1','2',3','4','5','6']
+      _images = [...?_images, ...fetchImages];
+
+      print(_images);
+    } catch (err) {
+      _images = [];
+      print("ERROR: $err");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   /// 1. FutureBuilder
   /// 2. Programar mediante condiciones
   /// 3. Gestor de estados
+  ///
+
+  /// Como controlar un scroll
+  /// 1. Usando el NotificationListener;
+  /// 2. Usando los controladores;
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_listener);
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Infinity Scroll')),
+      appBar: AppBar(title: const Text('Infinity Scroll'), actions: [
+        TextButton(
+          onPressed: () {
+            _scrollController.animateTo(0,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.bounceIn);
+          },
+          child: const Text('Inicio'),
+        ),
+        TextButton(
+          onPressed: () {
+            _scrollController.animateTo(
+                _scrollController.position.maxScrollExtent,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.bounceIn);
+          },
+          child: const Text('Final'),
+        ),
+      ]),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _images = [];
+          initialImages = 0;
+          await _getImages();
+        },
+        child: Builder(
+          builder: (context) {
+            if (_isLoading && _images == null) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (_images!.isEmpty) {
+              return const Center(
+                child: Icon(Icons.ac_unit),
+              );
+            }
+
+            /// Uso del notification listener
+            // return NotificationListener<ScrollNotification>(
+            //   onNotification: (notification) {
+            //     // print(notification.metrics.maxScrollExtent);
+            //     // print(notification.metrics.pixels);
+            //     //imprima un valor cuando nos falte 50 pixeles para llegar al fondo del scroll
+            //     if (notification.metrics.pixels + 50 >=
+            //             notification.metrics.maxScrollExtent &&
+            //         !_isLoading) {
+            //       print("PUEDES HACER FETCH");
+            //       initialImages = initialImages + amount;
+            //       _getImages();
+            //     }
+            //     return true;
+            //   },
+            //   child: Column(
+            //     children: [
+            //       if (_isLoading) const LinearProgressIndicator(),
+            //       Expanded(
+            //         child: ListView.builder(
+            //           // physics: BouncingScrollPhysics(),
+            //           itemBuilder: (context, index) {
+            //             final image = _images![index];
+            //             return ImageCard(image: image);
+            //           },
+            //           itemCount: _images!.length,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // );
+            /// Uso del controller
+            return Column(
+              children: [
+                if (_isLoading) const LinearProgressIndicator(),
+                Expanded(
+                  child: ListView.builder(
+                    controller: _scrollController,
+                    // physics: BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final image = _images![index];
+                      return ImageCard(image: image);
+                    },
+                    itemCount: _images!.length,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
       // body: FutureBuilder(
       //   future: Services().getImages(0, 10),
       //   builder: (context, snapshot) {
